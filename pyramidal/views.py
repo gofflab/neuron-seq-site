c1fcbcb1c0af1f6ef71faedef427190c48d1fd9d
wilkie <wilkie05@gmail.com>
2013-11-22 06:34:28 -0500

    Fixes json downloading to work in both python 2 and 3.

--- pyramidal/views.py
+++ pyramidal/views.py
@@ -5,7 +5,23 @@ from django.shortcuts import render
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-import json, urllib2
+import json
+
+try:
+  # python 3
+  from urllib.request import urlopen
+  def pullJson(url):
+    response = urlopen(url)
+    encoding = response.headers.get_content_charset()
+    data = json.loads(response.read().decode(encoding))
+    return data
+except ImportError:
+  # python 2
+  from urllib2 import urlopen
+  def pullJson(url):
+    response = urlopen(url)
+    data = json.load(response)
+    return data
 
 from pyramidal.models import Gene,Isoform
 
@@ -66,8 +82,7 @@ def getAllenExperimentIds(gene_id='Fezf2'):
 	# DevMouse
 	#baseUrl = "http://api.brain-map.org/api/v2/data/SectionDataSet/query.json?criteria=[failed$eqfalse],products[abbreviation$eqDevMouse],genes[acronym$eq%s]" % gene_id
 	#print baseUrl
-	response = urllib2.urlopen(baseUrl)
-	data = json.load(response)
+	data = pullJson(baseUrl)
 	if data['success']:
 		res = [x['id'] for x in data['msg']]
 	else:
@@ -77,8 +92,7 @@ def getAllenExperimentIds(gene_id='Fezf2'):
 def getAllenSectionData(gene_id='Fezf2'):
 	#baseUrl = "http://api.brain-map.org/api/v2/data/SectionDataSet/query.json?criteria=[failed$eqfalse],products[abbreviation$eqDevMouse],plane_of_section[name$eq'coronal'],genes[acronym$eq'%s']&include=genes,section_images,specimen(donor(age))" % gene_id
 	baseUrl = "http://api.brain-map.org/api/v2/data/SectionDataSet/query.json?criteria=[failed$eqfalse],products[abbreviation$eqDevMouse],genes[acronym$eq'%s']&include=genes,section_images,specimen(donor(age))" % gene_id
-	response = urllib2.urlopen(baseUrl)
-	data = json.load(response)
+	data = pullJson(baseUrl)
 	if data['success']:
 		return data
 	else:
@@ -129,50 +143,50 @@ import re
 from django.db.models import Q
 
 def normalize_query(query_string,
-                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
-                    normspace=re.compile(r'\s{2,}').sub):
-    ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
-        and grouping quoted words together.
-        Example:
+	                  findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+	                  normspace=re.compile(r'\s{2,}').sub):
+	  ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
+	      and grouping quoted words together.
+	      Example:
 
-        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
-        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+	      >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+	      ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
 
-    '''
-    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+	  '''
+	  return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
 
 def get_query(query_string, search_fields):
-    ''' Returns a query, that is a combination of Q objects. That combination
-        aims to search keywords within a model by testing the given search fields.
+	  ''' Returns a query, that is a combination of Q objects. That combination
+	      aims to search keywords within a model by testing the given search fields.
 
-    '''
-    query = None # Query to search for every search term
-    terms = normalize_query(query_string)
-    for term in terms:
-        or_query = None # Query to search for a given term in each field
-        for field_name in search_fields:
-            q = Q(**{"%s__icontains" % field_name: term})
-            if or_query is None:
-                or_query = q
-            else:
-                or_query = or_query | q
-        if query is None:
-            query = or_query
-        else:
-            query = query & or_query
-    return query
+	  '''
+	  query = None # Query to search for every search term
+	  terms = normalize_query(query_string)
+	  for term in terms:
+	      or_query = None # Query to search for a given term in each field
+	      for field_name in search_fields:
+	          q = Q(**{"%s__icontains" % field_name: term})
+	          if or_query is None:
+	              or_query = q
+	          else:
+	              or_query = or_query | q
+	      if query is None:
+	          query = or_query
+	      else:
+	          query = query & or_query
+	  return query
 
 
 ##############
 # Search
 ##############
 def search(request):
-    query_string = ''
-    found_genes = None
-    if ('q' in request.GET) and request.GET['q'].strip():
-        query_string = request.GET['q']
+	  query_string = ''
+	  found_genes = None
+	  if ('q' in request.GET) and request.GET['q'].strip():
+	      query_string = request.GET['q']
 
-        entry_query = get_query(query_string, ['gene_id', 'gene_short_name',])
+	      entry_query = get_query(query_string, ['gene_id', 'gene_short_name',])
 
         found_genes = Gene.objects.filter(entry_query).order_by('gene_id')
     return render(request,'pyramidal/search_results.html',
