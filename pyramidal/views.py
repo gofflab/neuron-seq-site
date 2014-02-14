f5bff7f0305219db6bdaf1553026df65b9c44c19
Loyale <loyalgoff@gmail.com>
2014-02-14 07:51:52 -0500

    Added Transcription Factor enrichment matrix

--- pyramidal/views.py
+++ pyramidal/views.py
@@ -11,49 +11,52 @@ from pyramidal.allen import AllenExplorer
 
 import json
 
+from numpy import zeros, array
+from math import sqrt, log
+
 def index(request):
-	context = {}
-	return render(request,'pyramidal/index.html',context)
+    context = {}
+    return render(request,'pyramidal/index.html',context)
 
 def geneIndex(request):
-	if ('order_by' in request.GET) and request.GET['order_by'].strip():
-		order_by = request.GET.get('order_by','defaultOrderField')
-	else:
-		order_by = 'gene_id'
-	allGenes = Gene.objects.all().order_by(order_by)
-	paginator = Paginator(allGenes,20) # Show 20 genes per page
-	page = request.GET.get('page')
-	try:
-		genes = paginator.page(page)
-	except PageNotAnInteger:
-		#If page is not an integer, deliver first page
-		genes = paginator.page(1)
-	except EmptyPage:
-		#If page is out of range, deliver last page of results
-		genes = paginator.page(paginator.num_pages)
-	context = {
-		'genes': genes,
-	}
-	return render(request,'pyramidal/geneIndex.html',context)
+    if ('order_by' in request.GET) and request.GET['order_by'].strip():
+        order_by = request.GET.get('order_by','defaultOrderField')
+    else:
+        order_by = 'gene_id'
+    allGenes = Gene.objects.all().order_by(order_by)
+    paginator = Paginator(allGenes,20) # Show 20 genes per page
+    page = request.GET.get('page')
+    try:
+        genes = paginator.page(page)
+    except PageNotAnInteger:
+        #If page is not an integer, deliver first page
+        genes = paginator.page(1)
+    except EmptyPage:
+        #If page is out of range, deliver last page of results
+        genes = paginator.page(paginator.num_pages)
+    context = {
+        'genes': genes,
+    }
+    return render(request,'pyramidal/geneIndex.html',context)
 
 def geneset(request,gene_list):
-	"""Takes a '+'-separated list of genes as <gene_list>"""
-	gene_list_clean = gene_list.rstrip().split("+")
-	try:
-		genes = Gene.objects.filter(gene_id__in=gene_list_clean)
-		expression = []
-		diffData = {}
-		for gene in genes:
-			expression.append(gene.expression())
-			diffData[gene.gene_id]=gene.diffData()
-	except Gene.DoesNotExist:
-		raise Http404
-	context = {
-		'genes': genes,
-		'diffData': json.dumps(diffData,separators=(',',':')),
-		'expression' :json.dumps(expression,separators=(',',':'))
-	}
-	return render(request,'pyramidal/geneset.html',context)
+    """Takes a '+'-separated list of genes as <gene_list>"""
+    gene_list_clean = gene_list.rstrip().split("+")
+    try:
+        genes = Gene.objects.filter(gene_id__in=gene_list_clean)
+        expression = []
+        diffData = {}
+        for gene in genes:
+            expression.append(gene.expression())
+            diffData[gene.gene_id]=gene.diffData()
+    except Gene.DoesNotExist:
+        raise Http404
+    context = {
+        'genes': genes,
+        'diffData': json.dumps(diffData,separators=(',',':')),
+        'expression' :json.dumps(expression,separators=(',',':'))
+    }
+    return render(request,'pyramidal/geneset.html',context)
 
 ##################
 # Gene & Isoform detail
@@ -106,16 +109,16 @@ def geneIsoforms(request, gene_id):
   return render(request,'pyramidal/geneShow.html',context)
 
 def isoformDetail(request,gene_id,isoform_id):
-	try:
-		# Get Isoform object
-		isoform = Isoform.objects.get(isoform_id=isoform_id)
+    try:
+        # Get Isoform object
+        isoform = Isoform.objects.get(isoform_id=isoform_id)
 
-		context = {
-			'isoform': isoform,
-		}
-		return render(request,'pyramidal/isoformDetail.html',context)
-	except Gene.DoesNotExist:
-		raise Http404
+        context = {
+            'isoform': isoform,
+        }
+        return render(request,'pyramidal/isoformDetail.html',context)
+    except Gene.DoesNotExist:
+        raise Http404
 
 def clusterIndex(request):
   context = {
@@ -136,52 +139,52 @@ import re
 from django.db.models import Q
 
 def normalize_query(query_string,
-	                  findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
-	                  normspace=re.compile(r'\s{2,}').sub):
-	  ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
-	      and grouping quoted words together.
-	      Example:
+                      findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                      normspace=re.compile(r'\s{2,}').sub):
+      ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
+          and grouping quoted words together.
+          Example:
 
-	      >>> normalize_query('  some random  words "with   quotes  " and   spaces')
-	      ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+          >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+          ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
 
-	  '''
-	  return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+      '''
+      return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
 
 def get_query(query_string, search_fields):
-	  ''' Returns a query, that is a combination of Q objects. That combination
-	      aims to search keywords within a model by testing the given search fields.
+      ''' Returns a query, that is a combination of Q objects. That combination
+          aims to search keywords within a model by testing the given search fields.
 
-	  '''
-	  query = None # Query to search for every search term
-	  terms = normalize_query(query_string)
-	  for term in terms:
-	      or_query = None # Query to search for a given term in each field
-	      for field_name in search_fields:
-	          q = Q(**{"%s__icontains" % field_name: term})
-	          if or_query is None:
-	              or_query = q
-	          else:
-	              or_query = or_query | q
-	      if query is None:
-	          query = or_query
-	      else:
-	          query = query & or_query
-	  return query
+      '''
+      query = None # Query to search for every search term
+      terms = normalize_query(query_string)
+      for term in terms:
+          or_query = None # Query to search for a given term in each field
+          for field_name in search_fields:
+              q = Q(**{"%s__icontains" % field_name: term})
+              if or_query is None:
+                  or_query = q
+              else:
+                  or_query = or_query | q
+          if query is None:
+              query = or_query
+          else:
+              query = query & or_query
+      return query
 
 def parseLocusQuery(query_string):
-	chrom, ranges = query_string.strip().split(":")
-	start,end = ranges.strip().split("-")
-	return [chrom,start,end]
+    chrom, ranges = query_string.strip().split(":")
+    start,end = ranges.strip().split("-")
+    return [chrom,start,end]
 
 def findGenesFromLocus(chrom,start,end):
-	found_features = Features.objects.filter(seqnames__iexact=chrom
-		).filter(end__gte=start
-		).filter(start__lte=end)
-	gene_ids = [f.gene_id for f in found_features]
-	gene_ids = set(gene_ids)
-	found_genes = Gene.objects.filter(gene_id__in=gene_ids).order_by('gene_id')
-	return found_genes
+    found_features = Features.objects.filter(seqnames__iexact=chrom
+        ).filter(end__gte=start
+        ).filter(start__lte=end)
+    gene_ids = [f.gene_id for f in found_features]
+    gene_ids = set(gene_ids)
+    found_genes = Gene.objects.filter(gene_id__in=gene_ids).order_by('gene_id')
+    return found_genes
 
 ##############
 # Search
@@ -193,12 +196,12 @@ def search(request):
     query_string = request.GET['q']
 
   if query_string.find(":")!=-1:
-  	query_string = query_string.replace(",","")
-  	chrom,start,end = parseLocusQuery(query_string)
-  	found_genes = findGenesFromLocus(chrom,start,end)
+    query_string = query_string.replace(",","")
+    chrom,start,end = parseLocusQuery(query_string)
+    found_genes = findGenesFromLocus(chrom,start,end)
   else:
-  	entry_query = get_query(query_string, ['gene_id', 'gene_short_name',])
-  	found_genes = Gene.objects.filter(entry_query).order_by('gene_id')
+    entry_query = get_query(query_string, ['gene_id', 'gene_short_name',])
+    found_genes = Gene.objects.filter(entry_query).order_by('gene_id')
 
   return render(request,'pyramidal/search_results.html',
         { 'query_string': query_string, 'found_genes': found_genes }
@@ -208,29 +211,68 @@ def search(request):
 # Development
 #####################
 def dev(request):
-	context = {}
-	return render(request,'pyramidal/testKegg.html',context)
+    context = {}
+    return render(request,'pyramidal/testKegg.html',context)
 
 ####################
 # Markers
 #####################
-def markers(request):
-	context = {}
-	return render(request,'base.html',context)
+def markers(request,expProfile,sigFlag,nGenes):
+    #Sample Order
+    sampleOrder=["E15_cpn","E15_subcereb","E15_corticothal","E16_cpn","E16_subcereb","E16_corticothal","E18_cpn","E18_subcereb","E18_corticothal","P1_cpn","P1_subcereb","P1_corticothal",]
+
+    #Clean expProfile
+    expProfile = makeProb(expProfile)
+
+    #Find JS distance values for each gene
+    allGenes = Gene.objects.all()
+    JSD = {}
+    for gene in allGenes:
+        geneDict = gene.fpkm()
+        geneProfile = []
+        for sample in sampleOrder:
+            geneProfile.append(geneDict[sample])
+        try:
+            geneProfile = makeProb(geneProfile)
+        except ZeroDivisionError:
+            continue
+        #print >>sys.stderr, "%s:\n%s" % (gene.gene_id,"\t".join([str(x) for x in geneProfile]))
+        JSD[gene.gene_id] = sqrt(Jensen_Shannon_divergence(geneProfile,expProfile))
+    #return JSD
+
+
+    context = {}
+    return render(request,'base.html',context)
+
+def KL_divergence(p, q):
+        return sum(_p * log(_p / _q) for _p, _q in zip(p, q) if _p != 0)
+
+def makeProb(p):
+     return [x/float(sum(p)) for x in p]
+
+def Jensen_Shannon_divergence(p, q):
+    JSD = 0.0
+    weight = 0.5
+    average = zeros(len(p))
+    for x in range(len(p)):
+        average[x] = weight * p[x] + (1 - weight) * q[x]
+        JSD = (weight * KL_divergence(array(p), average)) + ((1 - weight) * KL_divergence(array(q), average))
+    return (1-(JSD/sqrt(2 * log(2))))
+
 
 ####################
 # TFBS
 #####################
 def tfbs(request):
-	context = {}
-	return render(request,'base.html',context)
+    context = {}
+    return render(request,'pyramidal/tfbs.html',context)
 
 ####################
 # Supplement
 #####################
 def supplement(request):
-	context = {}
-	return render(request,'pyramidal/supplement.html',context)
+    context = {}
+    return render(request,'pyramidal/supplement.html',context)
 
 
 
